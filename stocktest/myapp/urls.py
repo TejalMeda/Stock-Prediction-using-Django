eb4f089cc5606ace497202dd9dd7271d57b8e12d
TejalMeda <93028909+TejalMeda@users.noreply.github.com>
2023-08-06 11:24:15 +0530

    Delete stocktest directory

--- stocktest/myapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.stock_prediction, name='stock_prediction'),
-    path('news/', views.stock_prediction, name='stock_prediction'),
-    
-]
--- stocktest/stocktest/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-
-urlpatterns = [
-    path('stock/', include('myapp.urls')),
-    
-]
